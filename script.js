60ff9cd1b2abab01b8325c71dd74ea3c601630dd
Raquel Cadete <57494565+RLCadete05@users.noreply.github.com>
2021-07-12 15:06:54 -0300

    part5

--- script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var rectangle = document.getElementById('rectangle');
-  window.addEventListener('scroll', function () {
-      if (window.scrollY > 0) rectangle.classList.add('rectangle-after');
-      else rectangle.classList.remove('rectangle-after');
-  });
-})();
